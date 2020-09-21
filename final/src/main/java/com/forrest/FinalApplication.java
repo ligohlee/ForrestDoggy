06af7e5a0ea1fd12966920ee7f440ca763c2d14f
hogil <hogil@Ligoh>
2020-09-21 12:04:43 +0900

    test

---
+++ final/src/main/java/com/forrest/FinalApplication.java
@@ -0,0 +1,13 @@
+package com.forrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApplication.class, args);
+	}
+
+}

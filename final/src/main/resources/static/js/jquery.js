d3f7f2730fbc084cc47e45d79102c7b6df08d49d
goott <goott@goott-PC>
2020-09-22 16:49:38 +0900

    재준 회원가입

--- final/src/main/resources/static/js/jquery.js
+++ final/src/main/resources/static/js/jquery.js
@@ -1,46 +1,53 @@
 $(function(){
 
   $('#toStayPark').mouseover(function(){
-    $('#toStayPark').animate({width:'100%'},500);
+    $('#toStayPark').stop().animate({width:'100%'},500);
   });
   $('#toStayPark').mouseleave(function(){
-    $('#toStayPark').animate({width:'25%'},200);
+    $('#toStayPark').stop().animate({width:'25%'},200);
   });
 
   $('#toDogVillage').mouseover(function(){
-    $('#toStayPark').animate({width:'0%'},500);
-    $('#toDogVillage').animate({width:'100%'},500);
+    $('#toStayPark').stop().animate({width:'0%'},500);
+    $('#toDogVillage').stop().animate({width:'100%'},500);
   });
   $('#toDogVillage').mouseleave(function(){
-    $('#toStayPark').animate({width:'25%'},200);
-    $('#toDogVillage').animate({width:'25%'},200);
+    $('#toStayPark').stop().animate({width:'25%'},200);
+    $('#toDogVillage').stop().animate({width:'25%'},200);
 
   });
 
   $('#toEatSomePlace').mouseover(function(){
-    $('#toStayPark').animate({width:'0%'},500);
-    $('#toDogVillage').animate({width:'0%'},500);
-    $('#toEatSomePlace').animate({width:'100%'},500);
+    $('#toStayPark').stop().animate({width:'0%'},500);
+    $('#toDogVillage').stop().animate({width:'0%'},500);
+    $('#toEatSomePlace').stop().animate({width:'100%'},500);
   });
   $('#toEatSomePlace').mouseleave(function(){
-    $('#toStayPark').animate({width:'25%'},200);
-    $('#toDogVillage').animate({width:'25%'},200);
-    $('#toEatSomePlace').animate({width:'25%'},500);
+    $('#toStayPark').stop().animate({width:'25%'},200);
+    $('#toDogVillage').stop().animate({width:'25%'},200);
+    $('#toEatSomePlace').stop().animate({width:'25%'},500);
 
   });
 
   $('#toDogMall').mouseover(function(){
-    $('#toStayPark').animate({width:'0%'},500);
-    $('#toDogVillage').animate({width:'0%'},500);
-    $('#toEatSomePlace').animate({width:'0%'},500);
-    $('#toDogMall').animate({width:'100%'},500);
+    $('#toStayPark').stop().animate({width:'0%'},500);
+    $('#toDogVillage').stop().animate({width:'0%'},500);
+    $('#toEatSomePlace').stop().animate({width:'0%'},500);
+    $('#toDogMall').stop().animate({width:'100%'},500);
   });
   $('#toDogMall').mouseleave(function(){
-    $('#toStayPark').animate({width:'25%'},200);
-    $('#toDogVillage').animate({width:'25%'},200);
-    $('#toEatSomePlace').animate({width:'25%'},500);
-    $('#toDogMall').animate({width:'25%'},500);
+    $('#toStayPark').stop().animate({width:'25%'},200);
+    $('#toDogVillage').stop().animate({width:'25%'},200);
+    $('#toEatSomePlace').stop().animate({width:'25%'},500);
+    $('#toDogMall').stop().animate({width:'25%'},500);
 
+  });
+
+  $('.select').mouseover(function(){
+    $('.menu').css('display','block');
+  });
+  $('.select').mouseleave(function(){
+    $('.menu').css('display','none');
   });
 
 

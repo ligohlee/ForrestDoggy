b8c5d5b8c65901b7d877e811f4a72d34457f52c6
hogil <hogil@Ligoh>
2020-10-05 10:47:30 +0900

    backoffice html folder 나눔 controller 경로변경

--- final/src/main/java/forrest/controller/backoffice/staypark/main/StayParKBackMainController.java
+++ final/src/main/java/forrest/controller/backoffice/staypark/main/StayParKBackMainController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("memberback")
-public class MemberBackMainController {
+@RequestMapping("stpback")
+public class StayParKBackMainController {
 	@RequestMapping(value = "main", method = RequestMethod.GET)
 	public String stpMain() {
-		return "thymeleaf/backOfficePage/html/member_backMain";
+		return "thymeleaf/backOfficePage/html/stp_manager/stp_backMain";
 	}
 }

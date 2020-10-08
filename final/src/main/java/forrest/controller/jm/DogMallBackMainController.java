06fa56baa235fc34f88493c6b2b7854e9854e90b
Admin <Admin@DESKTOP-AG2PK1N>
2020-10-08 14:53:46 +0900

    goods list / partner list mody del add

--- final/src/main/java/forrest/controller/jm/DogMallBackMainController.java
+++ final/src/main/java/forrest/controller/jm/DogMallBackMainController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 @RequestMapping("dgmback")
 public class DogMallBackMainController {
+	
 	@RequestMapping(value = "main", method = RequestMethod.GET)
-	public String stpMain() {
+	public String dgmMain() {
 		return "thymeleaf/backOfficePage/html/dgm_manager/dgm_backMain";
 	}
+	
+	
 }
--- final/src/main/java/forrest/mapper/jm/PartnerMapper.java
+++ final/src/main/java/forrest/mapper/jm/PartnerMapper.java
@@ -2,9 +2,18 @@ package forrest.mapper.jm;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 import forrest.domain.jm.PartnerDTO;
+import forrest.domain.jm.StartEndPageDTO;
 
+@Component
+@Repository
 public interface PartnerMapper {
-	public List<PartnerDTO> getPartnerList(PartnerDTO dto) throws Exception;
-
+	public List<PartnerDTO> getPartnerList(StartEndPageDTO startEndPageDTO) throws Exception;
+	public Integer getPartnerCount() throws Exception;
+	public void partnerUpdate(PartnerDTO dto) throws Exception;
+	public void partnerInsert(PartnerDTO dto) throws Exception;
+	public void partnerDelete(String ptNum) throws Exception;
 }

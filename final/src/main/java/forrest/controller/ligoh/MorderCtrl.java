fd56a61f0e18a20a7b0c55eee5f0e357b08f9b98
Ligoh <68572740+Hogil@users.noreply.github.com>
2020-10-22 21:33:49 +0900

    morder

--- final/src/main/java/forrest/controller/ligoh/MorderCtrl.java
+++ final/src/main/java/forrest/controller/ligoh/MorderCtrl.java
@@ -1,5 +1,23 @@
 package forrest.controller.ligoh;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import forrest.service.ligoh.morder.MorderListService;
+
+@Controller
+@RequestMapping("espback/order")
 public class MorderCtrl {
+	@Autowired
+	MorderListService morderListService;
+	
+	
+	@RequestMapping(value = "list")
+	public String menuOpen(Model model) throws Exception{
+		morderListService.getOrderList(model);
+		return "thymeleaf/backOfficePage/html/esp_manager/order/order_list";
+	}
 
 }

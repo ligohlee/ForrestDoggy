931718b0b577d7e50c0309c4205712308dbfe186
Ligoh <68572740+Hogil@users.noreply.github.com>
2020-10-22 17:53:27 +0900

    결제중

--- final/src/main/java/forrest/controller/ligoh/ESPMenuCtrl.java
+++ final/src/main/java/forrest/controller/ligoh/ESPMenuCtrl.java
@@ -8,11 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import forrest.command.ligoh.MorderCommand;
+import forrest.command.sy.PaymentCommand;
+import forrest.command.sy.PointCommand;
 import forrest.service.ligoh.course.CourseListService;
 import forrest.service.ligoh.menu.MenuListService;
 import forrest.service.ligoh.menu.MenuShowService;
 import forrest.service.ligoh.morder.BookShowService;
 import forrest.service.ligoh.morder.MakeABookService;
+import forrest.service.ligoh.morder.MenuPayService;
+import forrest.service.ligoh.point.LpointService;
+import forrest.service.sy.PaymentService;
+import forrest.service.sy.PointService;
 
 @Controller
 @RequestMapping("esp")
@@ -28,6 +34,15 @@ public class ESPMenuCtrl {
 	CourseListService courseListService;
 	@Autowired
 	BookShowService bookShowService;
+	@Autowired
+	LpointService lpointService;
+	@Autowired
+	PointService pointService;
+	@Autowired
+	PaymentService paymentService;
+	@Autowired
+	MenuPayService menuPayService;
+	
 	
 	
 	@RequestMapping(value = "menu")
@@ -48,12 +63,14 @@ public class ESPMenuCtrl {
 		makeABookService.firstRegist(mordCommand, request);
 		makeABookService.secondRegist(mordCommand, request);
 		bookShowService.sendInfo(model, request);
+		lpointService.pointShow(model, request);
 		return "thymeleaf/frontPage/html/ligoh/espPay";
 	}
 	@RequestMapping(value ="pay")
-	public String pay(MorderCommand mordCommand, HttpServletRequest request, Model model) throws Exception{
-		
-		model.addAttribute("courses", mordCommand.getCourseNum());
+	public String pay(PointCommand pointCommand, PaymentCommand paymentCommand, HttpServletRequest request, Model model) throws Exception{
+		pointService.insertPoint(pointCommand);
+		paymentService.insertPayment(paymentCommand);
+		menuPayService.insertMenuPay(request);
 		return "thymeleaf/frontPage/html/ligoh/espPayOk";
 	}
 
